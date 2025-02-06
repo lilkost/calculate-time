1482c6ae2dc24d5d5e0ac66ea1cae76dea0662ed
lilkost <111429255+lilkost@users.noreply.github.com>
2025-02-06 17:51:56 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -88,7 +88,7 @@ setBtn.addEventListener("click", ()=> setValue())
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
             .then((registration) => {
                 console.log('Service Worker зарегистрирован с областью:', registration.scope);
             })
